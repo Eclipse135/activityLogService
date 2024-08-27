6ae386d22e59ad350ee9600169514a1269a995d7
Eclipse <eclipse@live.com.pt>
2024-08-27 18:12:00 +0100

    Updated DemoController, it's path and the view in order to make the test easier

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -3,16 +3,39 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Attribute\Route;
+use function Symfony\Component\Clock\now;
 
 class DemoController extends AbstractController
 {
-    #[Route('/demo', name: 'app_demo')]
+    #[Route('/', name: 'app_demo')]
     public function index(): Response
     {
-        return $this->render('demo/index.html.twig', [
-            'controller_name' => 'DemoController',
-        ]);
+        $logs = [
+            [
+                'date' => now()->format('Y-m-d'),
+                'type' => 'test_type',
+                'message' => 'Hello World!',
+            ],
+        ];
+
+        return $this->render('demo/index.html.twig',
+            compact('logs'),
+        );
+    }
+
+    #[Route('/', name: 'app_demo_action', methods: ['POST'])]
+    public function action(Request $request)
+    {
+        $action = $request->request->get('action');
+
+        if (!$action) {
+            throw new BadRequestHttpException('Action required!');
+        }
+
+        return $this->redirectToRoute('app_demo');
     }
 }
