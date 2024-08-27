d540ccb27c3ee27d914102a72366ed5ea1a5e59b
Eclipse <eclipse@live.com.pt>
2024-08-27 15:39:03 +0100

    Created basic User Controller with views for index and show

---
+++ src/Controller/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/user')]
+class UserController extends AbstractController
+{
+    #[Route('/', name: 'app_user')]
+    public function index(): Response
+    {
+        // todo list users, with pagination
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user_show')]
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig',
+            compact('user')
+        );
+    }
+}
